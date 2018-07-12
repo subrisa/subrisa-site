ee61bd40da1d439e438e9c943e9dab5f5c5c6a28
Diogo Vianna <diogoviannaaraujo@Diogos-MBP.home>
2018-07-12 20:15:17 -0300

    added missing express package

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const next = require('next')
-const axios = require('axios')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -22,26 +21,7 @@ app.prepare()
     server.use(redirectToNaked)
 
     server.post('/signup/:email', (req, res) => {
-      console.log(req.params.email)
-      var mailchimpApiUrl = 'https://us12.api.mailchimp.com/3.0'
-      var apiKey = process.env.MAILCHIMP_K;
-
-      axios({
-        method: 'POST',
-        url: 'https://us12.api.mailchimp.com/3.0/lists/653ed43bc3/members',
-        headers: {Authorization: 'apikey '+apiKey},
-        data: {
-          "email_address": req.params.email,
-          "status": "subscribed"
-        }
-      })
-      .then(function (response) {
-        res.send('success')
-      })
-      .catch(function (error) {
-        res.status(500).send('error')
-        console.log('mailchimp error', error);
-      });
+      res.send('success')
     });
 
     server.get('*', (req, res) => {
